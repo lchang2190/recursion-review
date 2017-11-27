921d7e286eb120380f37a1be9f319687711188c3
lchang2190 <lchang2190@gmail.com>
2017-11-27 15:49:51 -0800

    solution for stringifyJSON and required alterations to the test file.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,48 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === undefined) {
+    return undefined;
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean'){
+    return obj.toString();
+  }
+  if (typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    var output = '[';
+    for (var i = 0; i < obj.length; i++) {
+      if (i === obj.length - 1) {
+        output += stringifyJSON(obj[i]);
+      } else {
+        output += stringifyJSON(obj[i]) + ','; 
+      }  
+    }
+    return output + ']'; 
+  }
+  if (typeof obj === 'object') {
+      var output = '';
+      for (var key in obj) {
+        if (typeof obj[key] === 'function' || obj[key] === undefined) {
+
+        } else { 
+          output += '"' + key + '"' + ':' + stringifyJSON(obj[key]) + ',';
+        }
+      }
+      // replaces last instance of a comma with nothing
+     output = output.replace(/,\s*$/, "");
+     return '{' + output + '}'
+    }
 };
+
+
+
+
+
+
+
+
+
+
