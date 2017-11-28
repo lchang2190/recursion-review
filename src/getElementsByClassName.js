9de6dd6020f603a0a8ecbbea2a904f26d49e0211
lchang2190 <lchang2190@gmail.com>
2017-11-27 20:37:26 -0800

    get elements by class name completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,87 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node) {
+  // create an empty array
+  //traverse all children of all DOM elements to find elements whose class name = classsName
+  // classList
+  var output = [];
+  node = node || document.body;
+  if (node.classList.contains(className)) {
+    output.push(node);
+  }
+  for (var i = 0; i < node.children.length; i++) {
+    var child = getElementsByClassName(className, node.children[i]);  
+    output = output.concat(child);
+  }
+  return output; 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* var elementsWithClass = [];
+  var children = document.childNodes;
+  var array = Array.prototype.slice.call(children); 
+  for (var i = 0; i < array.length; i++) {
+    if (hasClass(array[i], className)) {
+      elementsWithClass.push(array[i]);
+    }
+    if (!array[i].hasChildNodes()) {
+      if (hasClass(array[i], className)) {
+        elementsWithClass.push(array[i]);
+      }
+    } else {
+      return array[i].getElementsByClassName(className);
+    }
+  }
+
+  return elementsWithClass;
+  //output: array of all elements
+};
+  
+
+var hasClass = function(element, className) {
+  var classList = element.classList;
+  var classList = Array.prototype.slice.call(classList);
+  for (var i = 0; i < classList.length; i++) {
+    if (classList[i].className() === className) {
+      return true;   
+    }
+  }
+};*/
